dca9ece7ecc0f84ec8eeb77f2a487250a9b5b462
Ashraful-Mijan <ashrafulmijan5@gmail.com>
2021-06-22 12:41:08 +0600

    delete some comment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import { IntlProvider } from 'react-intl';
 import Layout from './Components/Layout';
-// import  messages from './Components/message';
 import './styles/App.scss';
 import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
-// messages={messages[locale]}
 function App() {
   const [locale, setLocale] = useState('en');
   return (
